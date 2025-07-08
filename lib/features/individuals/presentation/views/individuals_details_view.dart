4d5409ee20064e20bc18c1095d1aead0b2b9b3ab
AaronCS25 <aaron.camacho@utec.edu.pe>
2025-07-08 00:12:34 -0500

    fix: Correct import order in IndividualsDetailsView for consistency

--- lib/features/individuals/presentation/views/individuals_details_view.dart
+++ lib/features/individuals/presentation/views/individuals_details_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cvpr_face_recognition/config/config.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cvpr_face_recognition/features/shared/shared.dart';
 import 'package:cvpr_face_recognition/features/individuals/individuals.dart';
 
 class IndividualsDetailsView extends StatelessWidget {
